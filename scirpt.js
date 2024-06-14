27a1bebbcbcd2c5f47af4cdc5c3a9a4766f7ee0f
IANK <127850087+duallanpedro@users.noreply.github.com>
2024-06-14 10:19:48 -0300

    job

--- scirpt.js
+++ scirpt.js
@@ -17,6 +17,7 @@ const perguntas = [
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
+        
     },
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
